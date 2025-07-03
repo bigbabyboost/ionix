3a7588498e550ce5fd5ee796e5f498c946cdf99d
bigbabyboost <ah0199004@gmail.com>
2025-07-03 10:55:22 +0700

    Reapply "test"
    This reverts commit 7441081bd52df4772211f4ae9a2066069e2385ed.

--- home/editors/nvf.nix
+++ home/editors/nvf.nix
@@ -1,6 +1,9 @@
-{inputs, lib, ...}:
 {
-  imports = [ inputs.nvf.homeManagerModules.default ];
+  inputs,
+  lib,
+  ...
+}: {
+  imports = [inputs.nvf.homeManagerModules.default];
 
   programs.nvf = {
     enable = true;
@@ -9,11 +12,25 @@
         viAlias = true;
         vimAlias = true;
         diagnostics.enable = true;
+        options = {
+          ignorecase = true;
+          smartcase = true;
+          autoindent = true;
+          linebreak = true;
+          undofile = true;
+        };
         lsp = {
           enable = true;
           formatOnSave = true;
           trouble.enable = true;
-          lightbulb.enable = true;
+          lightbulb = {
+            enable = true;
+            autocmd.enable = true;
+          };
+          lspconfig.enable = true;
+          lspkind.enable = true;
+          null-ls.enable = true;
+          lspsaga.enable = true;
         };
         languages = {
           enableFormat = true;
@@ -22,8 +39,8 @@
           nix.enable = true;
           nix.lsp.server = "nixd";
           nix.format = {
-          	enable = true;
-          	type = "alejandra";
+            enable = true;
+            type = "alejandra";
           };
 
           clang.enable = true;
@@ -31,19 +48,28 @@
           markdown.enable = true;
           html.enable = true;
           lua.enable = true;
-          python.enable = true; 
+          python.enable = true;
         };
-        enableLuaLoader = true;
+        terminal.toggleterm = {
+          enable = true;
+          lazygit = {
+            enable = true;
+            mappings.open = "<leader>lg";
+          };
+        };
+        # enableLuaLoader = true;
         statusline.lualine.enable = true;
         autocomplete.nvim-cmp.enable = true;
         autocomplete = {
           blink-cmp.enable = true;
-          blink-cmp.setupOpts.signature.enabled = true;          
-          enableSharedCmpSources = true;
+          blink-cmp.setupOpts.signature.enabled = true;
         };
         autopairs.nvim-autopairs.enable = true;
         telescope.enable = true;
-        git.enable = true;        
+        git = {
+          enable = true;
+          git-conflict.enable = true;
+        };
         preventJunkFiles = true;
         undoFile.enable = true;
         clipboard = {
@@ -52,7 +78,6 @@
         };
         notes = {
           todo-comments.enable = true;
-          obsidian.enable = true;
         };
         visuals = {
           nvim-web-devicons.enable = true;
@@ -82,8 +107,19 @@
           };
           fastaction.enable = true;
         };
+        mini = {
+          align.enable = true;
+          pairs.enable = true;
+          basics.enable = true;
+          surround.enable = true;
+          notify.enable = true;
+          icons.enable = true;
+        };
+        snippets.luasnip.enable = true;
+        spellcheck.enable = true;
+        tabline.nvimBufferline.enable = true;
         comments.comment-nvim.enable = true;
       };
     };
-  };  
+  };
 }
