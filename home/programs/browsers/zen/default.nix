f4f81d949253a56b9912ca69b4086a5e2a58a58d
bigbabyboost <ah0199004@gmail.com>
2025-07-05 09:40:19 +0700

    remove startpage

--- home/programs/browsers/zen/default.nix
+++ home/programs/browsers/zen/default.nix
@@ -16,26 +16,15 @@
         search = {
           force = true;
           order = [
-            "Startpage"
+            "google"          
             "Searx"
             "NixOS Packages"
             "NixOS Options"
             "NixOS Wiki"
             "Home Manager Options"
-            "google"
           ];
-          default = "Startpage";
+          default = "google";
           engines = {
-            "Startpage" = {
-              urls = [
-                {
-                  template = "https://www.startpage.com/sp/search?query={searchTerms}&prfe=c602752472dd4a3d8286a7ce441403da08e5c4656092384ed3091a946a5a4a4c99962d0935b509f2866ff1fdeaa3c33a007d4d26e89149869f2f7d0bdfdb1b51aa7ae7f5f17ff4a233ff313d";
-                }
-              ];
-              icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-              definedAliases = ["@sp"];
-            };
-
             "Nix Packages" = {
               urls = [
                 {
